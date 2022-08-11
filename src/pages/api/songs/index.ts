95b9834a4d6f4accf4e132a758e6ca09f53987b1
juansecalvinio <j.s.calvinio@gmail.com>
2022-08-11 03:15:46 -0300

    Ready to deploy

--- src/pages/api/songs/index.ts
+++ src/pages/api/songs/index.ts
@@ -1,14 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { dbConnect } from "utils/mongoose"
-import { Song } from "models/Song"
 import { SongServices } from 'utils/services'
+import { Song } from "models/Song"
 
-// dbConnect()
+dbConnect()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   switch(req.method) {
     case "POST":
-      // const songs = await Song.find()
       try {
         const songs = await SongServices.instance.getAll()
         return res.status(200).json({ data: songs })
