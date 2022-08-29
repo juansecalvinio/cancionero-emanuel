26282ae0ec31ca462ecf3ffb4812cb7c17aec133
juansecalvinio <j.s.calvinio@gmail.com>
2022-08-29 14:17:21 -0300

    Deploy to development

--- src/styles/index.styled.ts
+++ src/styles/index.styled.ts
@@ -6,6 +6,14 @@ export const SongsContainerStyled = styled.div`
   margin-top: 1rem;
 `
 
+export const SpinnerContainerStyled = styled.div`
+  display: grid;
+  place-content: center;
+  justify-items: center;
+  gap: 1rem;
+  height: 100%;
+`
+
 export const FilterContainerStyled = styled.div`
   display: flex;
   flex-direction: column;
